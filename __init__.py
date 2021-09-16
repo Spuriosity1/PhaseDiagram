279378118f42ae2340abf4e9e518209d03350662
Spuriosity1 <alaric.sanders@gmail.com>
2021-09-16 17:33:07 +1000

    Working PhasePlane implementation

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from SpinWave import phase_optimise, plot_phasedia, PhasePlane, PhaseSphere
+
---
+++ examples/basic.py
@@ -0,0 +1,22 @@
+import sys, os
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+import numpy as np
+import matplotlib.pyplot as plt
+
+from PhaseDiagram import PhasePlane
+
+
+def phase(X, Y):
+    return np.where(X**2 + 4*Y**2 > 1, 1, 0) + np.where(X + Y > 0, 1, 0)
+
+p = PhasePlane(phase, ['A','B','C'], ['x1','x2'])
+p.set_initpts(np.linspace(-1.5,1.5,10),np.linspace(-1.5,1.5,10))
+
+if len(sys.argv) == 1:
+    p.refine(6)
+else:
+    p.refine(int(sys.argv[1]))
+
+p.plot()
+
+plt.show()
